4576f022488eae8cd89a299bc1a21f850557fbb5
oolayemi <olayemiolaomo5@gmail.com>
2020-06-15 01:43:49 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const questions = [
+      {
+        'questionText': 'What\'s your favourite color?',
+        'answer': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answer': ['Rabbit', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favourite instructor?',
+        'answer': ['Max', 'Max', 'Max', 'Max'],
+      },
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My title"),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
+        ),
+      ),
+    );
+  }
+}
